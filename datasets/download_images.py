84b4ab49c1d4ac9d2b0c9f72537a492da7766f89
Anthony Hu <anth.hu@gmail.com>
2017-08-10 21:12:10 +0100

    Added dataset_dir and subdir as arguments

--- datasets/download_images.py
+++ datasets/download_images.py
@@ -3,27 +3,29 @@ import os
 import pandas as pd
 import tensorflow as tf
 
-def download_im(search_query, start, end):
-    """Download images given the urls in the dataframe specified by the search_query
+def download_im(search_query, start, end, dataset_dir, subdir='photos'):
+    """Download images given the urls in the dataframe specified by a search query.
 
     Args:
-        search_query: A string giving the sentiment to load the corresponding dataframe
-        start: index to start downloading
-        end: index to end downloading
+        search_query: A string giving the sentiment of the photos to be downloaded
+        start: Index to start downloading
+        end: Index to finish downloading
+        dataset_dir: Directory where the dataframes are stored
+        subdir: Subdirectory to store the photos
 
     Returns:
-        Images downloaded in the directory data/photos/search_query, having the post id
-        as names.
+        Images downloaded in the directory dataset_dir/subdir/search_query, having 
+        the posts ids as names.
     """
     # Load previously scraped data
-    df = pd.read_csv(os.path.join('data', search_query + '.csv'), encoding='utf-8')
+    df = pd.read_csv(os.path.join(dataset_dir, search_query + '.csv'), encoding='utf-8')
     links = df['photo']
     ids = df['id']
-    dataset_dir = os.path.join('data/photos', search_query)
-
-    if not tf.gfile.Exists(dataset_dir):
-        tf.gfile.MakeDirs(dataset_dir)
+    photos_dir = os.path.join(dataset_dir, subdir, search_query)
+    if not tf.gfile.Exists(photos_dir):
+        tf.gfile.MakeDirs(photos_dir)
     for i in range(start, end):
-        # Check for NaN
+        # Check for NaNs
         if links[i] == links[i]:
-            urllib.urlretrieve(links[i], os.path.join(dataset_dir, str(ids[i]) + '.jpg'))
+            # Download photo
+            urllib.urlretrieve(links[i], os.path.join(photos_dir, str(ids[i]) + '.jpg'))
