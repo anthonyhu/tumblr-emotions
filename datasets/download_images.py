360a07c72c189d22fd5ea631d36c8ea5966f3baa
Anthony Hu <anth.hu@gmail.com>
2017-08-10 21:01:54 +0100

    Added documentation

--- datasets/download_images.py
+++ datasets/download_images.py
@@ -4,6 +4,17 @@ import pandas as pd
 import tensorflow as tf
 
 def download_im(search_query, start, end):
+    """Download images given the urls in the dataframe specified by the search_query
+
+    Args:
+        search_query: A string giving the sentiment to load the corresponding dataframe
+        start: index to start downloading
+        end: index to end downloading
+
+    Returns:
+        Images downloaded in the directory data/photos/search_query, having the post id
+        as names.
+    """
     # Load previously scraped data
     df = pd.read_csv(os.path.join('data', search_query + '.csv'), encoding='utf-8')
     links = df['photo']
