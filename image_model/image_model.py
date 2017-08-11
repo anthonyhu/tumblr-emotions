2b04a29a03745ef3954b50a50d1b267a05fc6e2c
Anthony Hu <anth.hu@gmail.com>
2017-08-11 18:28:27 +0100

    Added function to fine-tune a model

--- image_model/image_model.py
+++ image_model/image_model.py
@@ -5,27 +5,24 @@ import os
 import tensorflow as tf
 
 from datasets import dataset_utils
-from preprocessing import inception_preprocessing
+from datasets.convert_to_dataset import get_split
+from slim.preprocessing import inception_preprocessing
 from slim.nets import inception
-#from datasets import flowers
 
 from tensorflow.contrib import slim
 
-# Inception image size
-_IMAGE_SIZE = inception.inception_v1.default_image_size
-
 def download_pretrained_model(url, checkpoint_dir):
-	"""Download pretrained inception model and store it in checkpoint_dir.
+    """Download pretrained inception model and store it in checkpoint_dir.
 
-	Parameters:
-		url: The url containing the compressed model.
-		checkpoint_dir: The directory to save the model.
-	"""
-	if not tf.gfile.Exists(checkpoint_dir):
-		tf.gfile.MakeDirs(checkpoint_dir)
-	dataset_utils.download_and_uncompress_tarball(url, checkpoint_dir)
+    Parameters:
+        url: The url containing the compressed model.
+        checkpoint_dir: The directory to save the model.
+    """
+    if not tf.gfile.Exists(checkpoint_dir):
+        tf.gfile.MakeDirs(checkpoint_dir)
+    dataset_utils.download_and_uncompress_tarball(url, checkpoint_dir)
 
-def load_batch(dataset, batch_size=32, height=299, width=299, is_training=False):
+def _load_batch(dataset, batch_size=32, height=299, width=299, is_training=False):
     """Loads a single batch of data.
     
     Args:
@@ -55,9 +52,74 @@ def load_batch(dataset, batch_size=32, height=299, width=299, is_training=False)
 
     # Batch it up.
     images, images_raw, labels = tf.train.batch(
-          [image, image_raw, label],
-          batch_size=batch_size,
-          num_threads=1,
-          capacity=2 * batch_size)
+        [image, image_raw, label],
+        batch_size=batch_size,
+        num_threads=1,
+        capacity=2 * batch_size)
     
     return images, images_raw, labels
+
+def _get_init_fn(checkpoints_dir, model_name='inception_v1.ckpt'):
+    """Returns a function run by the chief worker to warm-start the training.
+    """
+    checkpoint_exclude_scopes=["InceptionV1/Logits", "InceptionV1/AuxLogits"]
+    
+    exclusions = [scope.strip() for scope in checkpoint_exclude_scopes]
+
+    variables_to_restore = []
+    for var in slim.get_model_variables():
+        excluded = False
+        for exclusion in exclusions:
+            if var.op.name.startswith(exclusion):
+                excluded = True
+                break
+        if not excluded:
+            variables_to_restore.append(var)
+
+    return slim.assign_from_checkpoint_fn(
+        os.path.join(checkpoints_dir, model_name),
+        variables_to_restore)
+
+def fine_tune_model(dataset_dir, checkpoints_dir, train_dir, num_steps):
+    """Fine tune the inception model, retraining the last layer.
+
+    Parameters:
+        dataset_dir: The directory containing the data.
+        checkpoints_dir: The directory contained the pre-trained model.
+        train_dir: The directory to save the trained model.
+        num_steps: The number of steps training the model.
+    """
+    if not tf.gfile.Exists(train_dir):
+        tf.gfile.MakeDirs(train_dir)
+
+    with tf.Graph().as_default():
+        tf.logging.set_verbosity(tf.logging.INFO)
+        
+        dataset = get_split('train', dataset_dir)
+        image_size = inception.inception_v1.default_image_size
+        images, _, labels = _load_batch(dataset, height=image_size, width=image_size)
+        
+        # Create the model, use the default arg scope to configure the batch norm parameters.
+        with slim.arg_scope(inception.inception_v1_arg_scope()):
+            logits, _ = inception.inception_v1(images, num_classes=dataset.num_classes, is_training=True)
+            
+        # Specify the loss function:
+        one_hot_labels = slim.one_hot_encoding(labels, dataset.num_classes)
+        slim.losses.softmax_cross_entropy(logits, one_hot_labels)
+        total_loss = slim.losses.get_total_loss()
+
+        # Create some summaries to visualize the training process:
+        tf.summary.scalar('losses/Total Loss', total_loss)
+      
+        # Specify the optimizer and create the train op:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+        train_op = slim.learning.create_train_op(total_loss, optimizer)
+        
+        # Run the training:
+        final_loss = slim.learning.train(
+            train_op,
+            logdir=train_dir,
+            init_fn=_get_init_fn(checkpoints_dir),
+            number_of_steps=num_steps)
+            
+    print('Finished training. Last batch loss %f' % final_loss)
