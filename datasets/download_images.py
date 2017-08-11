a86419936f565d041c62729bb2b46bc1d5a02f24
Anthony Hu <anth.hu@gmail.com>
2017-08-11 11:09:28 +0100

    Added handling URLError exception

--- datasets/download_images.py
+++ datasets/download_images.py
@@ -1,18 +1,19 @@
-import urllib
 import os
 import pandas as pd
 import tensorflow as tf
+import urllib2
+import io
 from PIL import Image
 
 def download_im(search_query, start, end, dataset_dir, subdir='photos'):
-    """Download images given the urls in the dataframe specified by a search query.
+    """Download images given the urls in the dataframe specified by the search query.
 
-    Args:
-        search_query: A string giving the sentiment of the photos to be downloaded.
-        start: Index to start downloading.
-        end: Index to finish downloading.
-        dataset_dir: Directory where the dataframes are stored.
-        subdir: Subdirectory to store the photos.
+    Parameters:
+        search_query: A string giving the sentiment to load the corresponding dataframe.
+        start: A start index for the loaded dataframe.
+        end: An end index for the loaded dataframe.
+        dataset_dir: A directory where the dataframes are stored.
+        subdir: A subdirectory to store the photos.
 
     Returns:
         Images downloaded in the directory dataset_dir/subdir/search_query, having 
@@ -22,20 +23,22 @@ def download_im(search_query, start, end, dataset_dir, subdir='photos'):
     df = pd.read_csv(os.path.join(dataset_dir, search_query + '.csv'), encoding='utf-8')
     links = df['photo']
     ids = df['id']
+    # Create subdir if it doesn't exist
+    if not tf.gfile.Exists(os.path.join(dataset_dir, subdir)):
+        tf.gfile.MakeDirs(os.path.join(dataset_dir, subdir))
+    # Create search_query folder if it doesn't exist
     photos_dir = os.path.join(dataset_dir, subdir, search_query)
     if not tf.gfile.Exists(photos_dir):
         tf.gfile.MakeDirs(photos_dir)
     for i in range(start, end):
         # Check for NaNs
         if links[i] == links[i]:
-            # The filename is: id + image extension
-            extension = links[i].split('.')[-1]
-            filename = str(ids[i]) + '.' + extension
-            # Download photo
-            urllib.urlretrieve(links[i], os.path.join(photos_dir, filename))
-            # Convert to .jpg if necessary and delete old filename
-            if extension != 'jpg':
-                im = Image.open(os.path.join(photos_dir, filename))
-                new_filename = str(ids[i]) + '.jpg'
-                im.convert('RGB').save(os.path.join(photos_dir, new_filename), 'JPEG')
-                os.remove(os.path.join(photos_dir, filename))
+            # Open url and convert to JPEG image
+            try:
+                f = urllib2.urlopen(links[i])
+            except Exception:
+                continue
+            image_file = io.BytesIO(f.read())
+            im = Image.open(image_file)
+            filename = str(ids[i]) + '.jpg'
+            im.convert('RGB').save(os.path.join(photos_dir, filename), 'JPEG')
